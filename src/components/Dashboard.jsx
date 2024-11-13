84f0a198b428e5c7e153f28452e145715d27e5a2
Choi-kanggun <kg4889@gmail.com>
2024-11-13 19:07:40 +0900

    포켓볼 이미지 경로 수정

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -27,7 +27,7 @@ const Dashboard = ({ selectedPokemon, setSelectedPokemon }) => {
         ))}
         {Array.from({ length: 6 - selectedPokemon.length }).map((_, idx) => (
           <MyPokemonCard key={idx}>
-            <MyPokemonImg src="../../public/pokeball.png"></MyPokemonImg>
+            <MyPokemonImg src="https://react-6-pokemon.vercel.app/assets/pokeball-13iwdk7Y.png"></MyPokemonImg>
           </MyPokemonCard>
         ))}
       </MyPokemonCardBox>
